66d3ef291991147356db3f20cc2be47c1b90d9d8
AlessandroBaldisser <alessandro.baldisser.01@gmail.com>
2024-09-30 19:56:11 +0200

    Class Conto

--- src/snacks/Conto.java
+++ src/snacks/Conto.java
@@ -1,5 +1,48 @@
 package snacks;
 
 public class Conto {
+	private int numeroConto;
+	private int saldo;
 	
+	Conto(int numeroConto, int saldo){
+		this.numeroConto = numeroConto;
+		this.saldo = saldo;
+	}
+	
+	// GETTERS
+	
+	public int getNumeroConto() {
+		return numeroConto;
+	}
+	
+	public int getSaldo() {
+		return saldo;
+	}
+	
+	// SETTERS 
+	
+	public void setNumeroConto(int numeroConto) {
+		this.numeroConto = numeroConto;
+	}
+	
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+	public int doDeposito(int deposito) {
+		saldo += deposito;
+		return saldo;
+	}
+	
+	public int doPrelievo(int prelievo) {
+		saldo -= prelievo;
+		return saldo;
+	}
+	
+	
+	public void saldoConto() {
+		System.out.println(this.saldo);
+	}
 }
+
--- src/snacks/Main.java
+++ src/snacks/Main.java
@@ -3,8 +3,12 @@ package snacks;
 public class Main {
 	public static void main(String [] args) {
 		Studente studente1 = new Studente("Truce", "Baldanzi", 22);
-		
+		Conto conto1 = new Conto(0,0);
 		System.out.println(studente1.presentati());
 		
+		conto1.doDeposito(1000);
+		conto1.saldoConto();
+		conto1.doPrelievo(500);
+		conto1.saldoConto();
 	}
 }
